e163382317f370a30db30aef145fe9caad5de486
JSBDeveloper <ids8600@naver.com>
2022-02-21 22:13:44 +0900

    Initial commit

---
+++ app/src/main/java/com/project/search_mvvm_livedata_viewmodel_coroutine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.project.search_mvvm_livedata_viewmodel_coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
